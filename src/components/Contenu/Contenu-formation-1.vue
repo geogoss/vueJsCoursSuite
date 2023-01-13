2c1d5f8d2b73104ab9be01d32cbc224682727792
geoffrey <geoffreygosset123@gmail.com>
2023-01-13 23:19:09 +0100

    preparation d'un nouveau dossier pour continuer le cours avec Enzo, j'ai fait deux component et une tite props pour repartir comme le formateur

---
+++ src/components/Contenu/Contenu-formation-1.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p class="m-5"> {{ txt }} </p>
+        <liste v-bind:prenom="prenom"></liste>
+    </div>
+</template>
+
+<script>
+
+import Liste from '../Liste/Liste-formation-1.vue'
+
+export default {
+    nom: 'contenu',
+    data() {
+        return {
+            txt: "Je suis Geoffrey Gosset",
+            prenom: "Franck Lucas"
+        }
+    },
+    components: {
+        'liste' : Liste
+    }
+}
+
+
+</script>
+
+<style scoped src="./Contenu-formation-1.css">
+
+</style>
---
+++ src/components/Liste/Liste-formation-1.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(fruit, index) in fruits" v-bind:key="index"> {{ fruit }} </li>
+        </ul>
+        <p>
+            {{ prenom }}
+        </p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        nom:'liste',
+        data() {
+            return {
+                fruits: ['fraise', 'citron', 'mangue', 'orange']
+            }
+        },
+        props: ['prenom']
+    }
+
+</script>
+
+<style scoped src="./Liste-formation-1.css"></style>
